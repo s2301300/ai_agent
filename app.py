94a3d4bd35db79384bdd0f1696ec7b55e578b01a
s2301300 <gimpo.s230130@ggh.goe.go.kr>
2024-11-19 15:05:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ import streamlit as st
 
 # 1.기계학습 모델 파일 로드
 import joblib
-# model = joblib.load('linear_regression_model.pkl')
+ model = joblib.load('linear_regression_model.pkl')
 
 # 2.모델 설명
 st.title('행복도 예측 모델')
 st.subheader('모델 설명')
 st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
 st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
-st.write(' - 훈련    데이터 : *건')
-st.write(' - 테스트 데이터 : *건')
-st.write(' - 인공지능 모델 정확도 : ***')
+st.write(' - 훈련    데이터 : 337건')
+st.write(' - 테스트 데이터 : 144건')
+st.write(' - 인공지능 모델 정확도 : 0.23')
 
 
 def add_bg_from_url():
@@ -35,13 +35,13 @@ add_bg_from_url()
 col1, col2, col3 = st.columns(3)  
 with col1:
       st.subheader('데이터시각화1')
-     # st.image('시각화1.png' )   # 이미지 불러오기
+      st.image('시각화1.png' )   # 이미지 불러오기
 with col2:
       st.subheader('데이터시각화2')
-    #  st.image('시각화2.png' )   # 이미지 불러오기
+      st.image('시각화2.png' )   # 이미지 불러오기
 with col3:
       st.subheader('데이터시각화3')
-     # st.image('시각화3.png')   # 이미지 불러오기
+      st.image('시각화3.png')   # 이미지 불러오기
 
 # 4.모델 활용
 st.subheader('모델 활용')
@@ -55,7 +55,7 @@ e = st.selectbox(' 확인빈도 ', [0,1,2,3,4,5] )
 f = st.selectbox(' 우울 ', [0,1,2,3,4,5] )
                                                             # 사용자가  0,1 중에 선택
 
-#if st.button('행복도 예측'):            # 사용자가 '점수예측' 버튼을 누르면
- #       input_data = [[a,b,c,d,e,f]]     # 사용자가 입력한 a,b,c 를 input_data에 저장하고
-  #      p = model.predict(input_data)         # model이 예측한 값을 p에 저장한다
-   #     st.write('인공지능의 예측 점수는', p)
+if st.button('행복도 예측'):            # 사용자가 '점수예측' 버튼을 누르면
+        input_data = [[a,b,c,d,e,f]]     # 사용자가 입력한 a,b,c 를 input_data에 저장하고
+        p = model.predict(input_data)         # model이 예측한 값을 p에 저장한다
+        st.write('인공지능의 예측 점수는', p)
