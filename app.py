4e2b4f02840f93b4061f9622eaf44d9a5ca89d9e
s2301300 <gimpo.s230130@ggh.goe.go.kr>
2024-11-19 15:14:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -47,14 +47,15 @@ with col3:
 st.subheader('모델 활용')
 st.write('**** 다음을 입력하세요.. 인공지능이 당신의 행복도를 예측해 드립니다.')
 
-a = st.selectbox(' 목적없는 사용 ', [0,1,2,3,4,5])      #초기값은 0
-b = st.selectbox(' 방해 ', [0,1,2,3,4,5] )     # 초기값은 0.0
-c = st.selectbox(' 의존도 ', [0,1,2,3,4,5] )
-d = st.selectbox(' 비교 ', [0,1,2,3,4,5] )
-e = st.selectbox(' 확인빈도 ', [0,1,2,3,4,5] )
+a = st.selectbox(' 시간 ', [0,1,2,3,4,5])      #초기값은 0
+b = st.selectbox(' 목적없는 사용 ', [0,1,2,3,4,5] )     # 초기값은 0.0
+c = st.selectbox(' 방해 ', [0,1,2,3,4,5] )
+d = st.selectbox(' 의존도 ', [0,1,2,3,4,5] )
+e = st.selectbox(' 비교 ', [0,1,2,3,4,5] )
+f = st.selectbox(' 확인빈도 ', [0,1,2,3,4,5])
                                                             # 사용자가  0,1 중에 선택
 
 if st.button('행복도 예측'):            # 사용자가 '점수예측' 버튼을 누르면
-        input_data = [[a,b,c,d,e]]     # 사용자가 입력한 a,b,c 를 input_data에 저장하고
+        input_data = [[a,b,c,d,e,f]]     # 사용자가 입력한 a,b,c 를 input_data에 저장하고
         p = model.predict(input_data)         # model이 예측한 값을 p에 저장한다
         st.write('인공지능의 예측 점수는', p)
